92cacb9f5418b1d783d26fce3ccf6d65599a8d2d
Aqib Dev <Aqib Dev@DESKTOP-EOQIN6T>
2024-08-21 11:53:43 +0500

    New Changes Made 21-8-2024

--- EMSProject/Controllers/CategoryController.cs
+++ EMSProject/Controllers/CategoryController.cs
@@ -1,17 +1,139 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using EMSProject.Models;
 
 namespace EMSProject.Controllers
 {
     public class CategoryController : Controller
     {
+        private EMSEntitiesdb db = new EMSEntitiesdb();
+
         // GET: Category
         public ActionResult Index()
         {
+            return View(db.categories.ToList());
+        }
+
+        // GET: Category/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            category category = db.categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // GET: Category/Create
+        public ActionResult Create()
+        {
             return View();
+        }
+
+
+
+        [HttpGet]
+        public JsonResult GetCategories()
+        {
+            var categories = db.categories.Select(c => new
+            {
+                Id = c.id,
+                Name = c.name
+            }).ToList();
+
+            return Json(categories, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+        // POST: Category/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+
+        public ActionResult Create(category cate)
+        {
+           
+                db.categories.Add(cate);
+                db.SaveChanges();
+                return Json("Success");
+        }
+
+        // GET: Category/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            category category = db.categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Category/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name")] category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Category/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            category category = db.categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            category category = db.categories.Find(id);
+            db.categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ EMSProject/Scripts/JSCustom.js
@@ -0,0 +1,77 @@
+﻿$(document).ready(function () {
+    $('#tblcat').DataTable(); // Initialize DataTable after everything is loaded
+    $('#tbleqpt').DataTable(); // Initialize DataTable after everything is loaded
+});
+
+$(document).ready(function () {
+$("#btn-cate-add").click(function () {
+    var Name = $('#name').val();
+    
+    if (Name == "") {
+
+        alert("Please enter the name!!!");
+
+    } else {
+
+        $("#btn-cate-add").text("Saving...");
+        $.ajax({
+            url: "/Category/Create",
+            type: "POST",
+            dataType: 'json',
+            data: {
+                Name: Name,
+           
+            },
+            success: function (response) {
+                // Clear Input Fields
+                $("#name").val("");
+                alert("Data Inserted");
+
+
+                // Button Text Change
+                $("#btn-cate-add").text("Save");
+
+                loadData();
+            },
+            error: function (xhr, status, error) {
+                alert("Server Error !!!");
+            }
+        });
+    }
+
+});
+
+
+
+    function loadData() {
+        $.ajax({
+            url: '/category/GetCategories',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                var table = $('#tblcat').DataTable(); // Initialize DataTable
+
+                table.clear(); // Clear any existing data
+
+                var i = 0;
+                $.each(data, function (index, category) {
+                    i++;
+                    table.row.add([
+                        i,
+                        category.Name,
+                        '<a href="/category/Edit/' + category.Id + '" class="btn btn-sm btn-warning">Edit</a> | ' +
+                        '<a href="/category/Details/' + category.Id + '" class="btn btn-sm btn-info">Details</a> | ' +
+                        '<a href="/category/Delete/' + category.Id + '" class="btn btn-sm btn-danger">Delete</a>'
+                    ]).draw(false);
+                });
+            },
+            error: function (xhr, status, error) {
+                alert("An error occurred while loading data: " + error);
+            }
+        });
+    }
+
+    loadData();
+
+
+});
